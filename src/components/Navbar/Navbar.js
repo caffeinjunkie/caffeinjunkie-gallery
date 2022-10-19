1fc944431ac539057d04fe8e2c4b389ebfd21a09
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-10-19 21:53:16 +0700

    remove responsive header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,16 +2,9 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import { MENU_ITEMS } from './Navbar.config';
-import { getCurrentBreakpoint } from '../../utils/ScreenUtils';
 import InstagramIcon from '../../assets/svgIcons/InstagramIcon.component'
 
 export default function Navbar() {
-  let navbarMenu = getCurrentBreakpoint() ? MENU_ITEMS : MENU_ITEMS.reverse();
-  
-  React.useEffect(() => {
-    navbarMenu = getCurrentBreakpoint() ? MENU_ITEMS : MENU_ITEMS.reverse();
-  })
-  
   const renderSocialMedia = () => (
     <div
       className="flex gap-2 justify-center align-center self-center py-2 mr-8 w-40"
@@ -33,7 +26,7 @@ export default function Navbar() {
       justify-between align-center
       py-6
       border-solid border-b-2 border-slate 100">
-      {navbarMenu.map(renderMenu)}
+      {MENU_ITEMS.map(renderMenu)}
       {renderSocialMedia()}
     </nav>
   )
