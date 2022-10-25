d9d0aef73e295938a6ea09c475a8ea8de693f6f0
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-10-25 21:40:54 +0700

    not updating

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -3,5 +3,7 @@ const SanityClient = require('@sanity/client');
 export default SanityClient({
   projectId: 'tru3kf4f',
   dataset: 'production',
+  apiVersion: '2021-03-25',
+  token: 'skq345DLlbqiFD4u6SZsBuutJdgY5PtHU7Jn9AaDih7Zcf0IGJQmp7qPcd31MWSzOR5w6EjSv39cUWRa9jL6wjA95IkrEK1n7bQgpdfnsjySh1UxDwQ5pOQqPcbfpgOYCfq0vtI4mQcQogFrdgosV3Az7VUVONdSv3Nxv5Mhvhmowwc3YDX1',
   useCdn: false
 })
