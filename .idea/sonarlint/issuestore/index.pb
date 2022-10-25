7904b59bc9e6e945d76e0e294d0afa5d902be56f
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-10-25 22:02:55 +0700

    update env

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,9 +3,6 @@
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 ?
 .prettierignore,b/6/b69d8408b6c89ae66e712f0e6af68848291cfdfb
-:
-
-.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
 O
 .cache/commonjs/default-html.js,3/6/36b9e2b988d2fbe9e4e3bc502bcd80c65864dff9
 N
--- src/hooks/useMutation.js
+++ src/hooks/useMutation.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export default async function useMutation(input) {
-  const baseUrl = `https://tru3kf4f.api.sanity.io`;
+  const baseUrl = `https://${process.env.SA}.api.sanity.io`;
   const url = `${baseUrl}/v2021-03-25/data/mutate/production`;
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${process.env.SANITY_WRITE_TOKEN}`
+    'Authorization': `Bearer ${process.env.GATSBY_SANITY_WRITE_TOKEN}`
   }
   
   const payload = {
