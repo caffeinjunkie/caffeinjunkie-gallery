3d48d7a903c3bc8cf9ba32b3ca22effaddc28cfa
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-10-25 20:43:41 +0700

    remove config

--- src/hooks/useMutation.js
+++ src/hooks/useMutation.js
@@ -1,16 +1,11 @@
 import axios from 'axios'
 
-import { sanity } from '../config'
-
-const { projectId, dataset, writeToken, apiVersion } = sanity;
-
 export default async function useMutation(input) {
-  const baseUrl = `https://${projectId}.api.sanity.io`;
-  const url = `${baseUrl}/v${apiVersion}/data/mutate/${dataset}`;
-  
+  const baseUrl = `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io`;
+  const url = `${baseUrl}/v${process.env.SANITY_API_VERSION}/data/mutate/${process.env.SANITY_DATASET}`;
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${writeToken}`
+    'Authorization': `Bearer ${process.env.SANITY_WRITE_TOKEN}`
   }
   
   const payload = {
