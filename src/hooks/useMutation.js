f24888a99e51f535297f488d536458aba809de01
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-10-25 21:53:54 +0700

    remove env

--- src/hooks/useMutation.js
+++ src/hooks/useMutation.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default async function useMutation(input) {
-  const baseUrl = `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io`;
-  const url = `${baseUrl}/v${process.env.SANITY_API_VERSION}/data/mutate/${process.env.SANITY_DATASET}`;
+  const baseUrl = `https://tru3kf4f.api.sanity.io`;
+  const url = `${baseUrl}/v2021-03-25/data/mutate/production`;
   const headers = {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${process.env.SANITY_WRITE_TOKEN}`
