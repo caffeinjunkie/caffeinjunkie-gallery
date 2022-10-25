aa39c2dd9b2b0e56a1859f8ff8561bcebcbcc89b
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-10-25 22:23:00 +0700

    fix env

--- src/hooks/useMutation.js
+++ src/hooks/useMutation.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export default async function useMutation(input) {
-  const baseUrl = `https://${process.env.GATSBY_SANITY_PROJECT_ID}.api.sanity.io`;
-  const url = `${baseUrl}/v2021-03-25/data/mutate/${process.env.GATSBY_SANITY_DATASET}`;
+  const baseUrl = `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io`;
+  const url = `${baseUrl}/v2021-03-25/data/mutate/${process.env.SANITY_DATASET}`;
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${process.env.GATSBY_SANITY_WRITE_TOKEN}`
+    'Authorization': `Bearer ${process.env.SANITY_WRITE_TOKEN}`
   }
   
   const payload = {
