7e0c88f245d4554a45d4165fbd22ea7b2cbfca1d
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-10-25 21:35:52 +0700

    fix error in deployment

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -1,11 +1,7 @@
 const SanityClient = require('@sanity/client');
 
-const client = SanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  apiVersion: process.env.SANITY_API_VERSION,
-  token: process.env.SANITY_READ_TOKEN,
-  useCdn: true
+export default SanityClient({
+  projectId: 'tru3kf4f',
+  dataset: 'production',
+  useCdn: false
 })
-
-export default client;
